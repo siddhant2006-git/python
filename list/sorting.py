bbfa4836e7d77e5ee8e4f239081fcc7c4f5deba3
siddhant2006-git <siddhantbhatnagar11@gmail.com>
2026-06-05 22:03:15 +0530

    Add sorting and tuple demonstration scripts

---
+++ list/sorting.py
@@ -0,0 +1,38 @@
+# sorting for alphbate
+a=["apple" ,"banana", "cat","dog"]
+a.sort()
+print(a)
+
+# sorting for the number
+b=[1,2,4,5,6774,5,432,2,3]
+a.sort()
+print(b)
+
+# reverse
+c=[1,2,3,2,4,3,4,3,52,2342]
+c.reverse()
+print(c)
+
+# copy list
+d=[1,23,4,42,42,442,223]
+e=d.copy()
+print(e)
+
+#slice operator 
+f = [1, 23, 4, 42, 42, 442, 223]
+g= f[:]
+print(g)
+
+# join list 
+v1=[1,23,3,45,23,5,3]
+v2=[11,22,344,5,54332,22,3,3]
+v3=v1+v2
+print(v3)
+
+# join two list for looping
+z=[1,2,33,4,5,642,234,4]
+z1=[12,3,3,212,2,3,2,3,3]
+for x in z:
+  z.append(x)
+print()
+
---
+++ tuple/01basics.py
@@ -0,0 +1,7 @@
+# tuple - it is collection of the unchangeable elements
+# tuple can used for unchangeable. 
+a=("1","2","3","4")
+
+# length of tuple 
+b=("apple","banan","cat","rat")
+print(len(b))
