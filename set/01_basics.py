4ab398003cd1f24ae0f9ec6cc95344fce957a299
siddhant2006-git <siddhantbhatnagar11@gmail.com>
2026-06-11 00:47:58 +0530

    Enhance set demonstration with additional examples and clarify comments

--- set/01_basics.py
+++ set/01_basics.py
@@ -5,12 +5,30 @@ s={"apple","mango","rat"}
 print(s)
 
 # unchangeable-set  item cannot be  directly change . not changeable
-#s1={"chai","code","class"}
-#s1[0]="class"
-#print(s1)
+# s1={"chai","code","class"}
+# s1[0]="class"
+# print(s1)
 
-# set item can add and remove the new element .
+# set item can  be add and remove the new element with any indexing .
 d={"siddhant","krish","yash","sarthak"}
-d.add(1)
+# d.add(1)
 d.add("hello")
 print(d)
+
+# Duplicates Not Allowed
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset) # apple can be print only 1 time .
+
+# some condition is same true and 1 is same .
+a={"blue","car","red","green",1,True}
+print(a)
+
+# false and 0 is also same .
+v1={"red","blue","black", 0,False}
+print(v1)
+
+# get length of set 
+q1={"flag","red","blue"}
+
+print(len(q1))
+
