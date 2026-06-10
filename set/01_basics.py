ca9fe15ae36296b09b7a4c820725ea13bf910790
siddhant2006-git <siddhantbhatnagar11@gmail.com>
2026-06-11 00:36:30 +0530

    Add initial script demonstrating set creation, modification, and properties

---
+++ set/01_basics.py
@@ -0,0 +1,16 @@
+# set-is collection of unordered ,unchangeable,unindexing
+
+# unordered-set order cannot be fixed.
+s={"apple","mango","rat"}
+print(s)
+
+# unchangeable-set  item cannot be  directly change . not changeable
+#s1={"chai","code","class"}
+#s1[0]="class"
+#print(s1)
+
+# set item can add and remove the new element .
+d={"siddhant","krish","yash","sarthak"}
+d.add(1)
+d.add("hello")
+print(d)
