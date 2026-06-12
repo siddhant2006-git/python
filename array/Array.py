a491382d30006368968341fe293d0467b708e1a0
siddhant2006-git <siddhantbhatnagar11@gmail.com>
2026-06-12 22:58:56 +0530

    Add initial Array script demonstrating element count, append, pop, and remove operations

---
+++ array/Array.py
@@ -0,0 +1,24 @@
+# Array - it is collection of elements
+
+# len-number of elements present in the array
+a=[1,2,3,4,4,32,2,2,3,2,3,333.32]
+b=len(a)
+print(b)
+
+# append - add the element
+a1=[1,22,3,3,3,4332,3,2,3223,332,23,22,3,32,2,2,23,3343,]
+a1.append("car")
+print(a1)
+
+# pop-remove the element with index
+A1=[112.32,322.2,3,3,2,333,22,3]
+A1.pop(1)
+print(A1)
+
+# remove - delete the element with character .
+
+A2 = [112.32, 322.2, 3, 3, 2, 333, 22, 3]
+A2.pop(3)
+print(A2)
+
+
